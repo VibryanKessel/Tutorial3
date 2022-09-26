99f83ce291398911c17f7bacd23146ca4c780b24
ZERO <zero20022003@gmail.com>
2022-09-26 22:48:07 +0200

    Revert "Ex 7: create BaseButton with primary color"

--- vue-oauth-microsoft-graph/src/components/HomePage.vue
+++ vue-oauth-microsoft-graph/src/components/HomePage.vue
@@ -1,20 +1,14 @@
 <template>
     <div class="header">
         <h1>Here's the Home page !</h1>
-        <div class="buttons">
-            <base-button value="BaseButton with custom margin" />
-            <base-button disabled="true" value="Basebutton disabled" />
-        </div>
     </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
-import BaseButton from "./BaseButton.vue";
 
 export default defineComponent({
-    name: "HomePage",
-  components: { BaseButton },
+  name: "HomePage",
   props: {
   },
 });
@@ -23,10 +17,5 @@ export default defineComponent({
 <style scoped>
     .header{
         margin: 10em;
-    }
-    .buttons{
-        display: flex;
-        padding: 0 10px;
-        justify-content: space-between;
     }
 </style>
