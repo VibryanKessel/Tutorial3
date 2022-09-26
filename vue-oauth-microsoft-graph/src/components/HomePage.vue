66e3cee8ab1139c81489dcd51aa1774f377dcaf5
ZERO <zero20022003@gmail.com>
2022-09-26 16:02:13 +0200

    HomePage imported

--- vue-oauth-microsoft-graph/src/components/HomePage.vue
+++ vue-oauth-microsoft-graph/src/components/HomePage.vue
@@ -1,9 +1,13 @@
+<template>
+    <h1>Here's the Home page !</h1>
+</template>
+
 <script lang="ts">
 import { defineComponent } from "vue";
 
 export default defineComponent({
-  name: "Home",
-  components: {
+  name: "HomePage",
+  props: {
   },
 });
 </script>
