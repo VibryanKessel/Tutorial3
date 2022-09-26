6e70f18532bbe0a9bb92b28bb79941e042e9caec
ZERO <zero20022003@gmail.com>
2022-09-26 23:07:13 +0200

    Ex 8: color palette and prop for BaseButton

---
+++ vue-oauth-microsoft-graph/src/components/BaseButton.vue
@@ -0,0 +1,70 @@
+<template>
+  <button
+    :color="color"
+    :class="[disabled ? 'btn-disabled' : 'btn-hover', color]"
+  >
+    {{ value }}
+  </button>
+</template>
+
+<script>
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "BaseButton",
+  props: {
+    value: String,
+    disabled: Boolean,
+    color: {
+      default: "primary",
+      type: String,
+    },
+  },
+  watch: {
+    color: function (value) {
+      switch (value) {
+        case "warn":
+          return "warn";
+        case "danger": 
+          return  "danger";
+        default: "primary";
+    }
+}
+    } 
+})
+</script>
+
+<style scoped>
+    button{
+        color: white;
+        padding: 25px 100px;
+        border: 0;
+        border-radius: 2%;
+        font-weight: bold;
+        font-size: 15px;
+        transition: 1s;
+    }
+    .btn-disabled{
+        opacity: .5;    
+    }
+    .btn-disabled:hover{
+        cursor: not-allowed;
+    }
+    .btn-hover:hover{
+        background-color: white;
+        color: black;
+        border: 2px solid;
+        cursor: pointer;
+        transition: 1s;
+        box-shadow: rgba(0, 0, 0, 0.35) 0px 5px 15px;
+    }
+    .primary{
+        background-color: rgba(0,150,0,.8);
+    }
+    .warn{
+        background-color: rgba(190, 80, 0, 0.8);
+    }
+    .danger{
+        background-color: rgba(190, 0, 0, 0.8);
+    }
+</style>
--- vue-oauth-microsoft-graph/src/components/HomePage.vue
+++ vue-oauth-microsoft-graph/src/components/HomePage.vue
@@ -1,14 +1,20 @@
 <template>
     <div class="header">
         <h1>Here's the Home page !</h1>
+        <div class="buttons">
+            <base-button value="BaseButton with custom margin" />
+            <base-button disabled="true" value="Basebutton disabled" />
+        </div>
     </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
+import BaseButton from "./BaseButton.vue";
 
 export default defineComponent({
-  name: "HomePage",
+    name: "HomePage",
+  components: { BaseButton },
   props: {
   },
 });
@@ -17,5 +23,10 @@ export default defineComponent({
 <style scoped>
     .header{
         margin: 10em;
+    }
+    .buttons{
+        display: flex;
+        padding: 0 10px;
+        justify-content: space-between;
     }
 </style>
